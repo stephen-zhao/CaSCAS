1c74d1f5f006a7ca96958701ddf0b265ffb4b467
Yizhou Deng <yizhoudeng@gmail.com>
2017-06-21 23:40:34 -0400

    changed addable/multiplicable/exponentiable with algebraic

--- src/main/scala/org/cascas_project/cascas/mathobj/MathObj.scala
+++ src/main/scala/org/cascas_project/cascas/mathobj/MathObj.scala
@@ -2,56 +2,39 @@
 // MathObj.scala : CaSCAS Project
 //=============================================================================
 
-trait Addable {}
+trait Algebraic {}
 
-trait Multiplicable {}
-
-trait Exponentiable {}
 
 abstract class MathObj {
 	
 }
 
-class FunctionNode extends MathObj with Addable,
-									   Multiplicable,
-									   Exponentiable {
+class FunctionNode extends MathObj with Algebraic {
 	val fn: String,
 	val params: scala.collection.mutable[MathObj]
 }
 
-class AddNode extends MathObj with Addable,
-									Multiplicable,
-									Exponentiable {
-	val summands: scala.collection.mutable[Addable]
+class AddNode extends MathObj with Algebraic{
+	val summands: scala.collection.mutable[Algebraic]
 }
 
-class MultNode extends MathObj with Addable,
-									Multiplicable,
-									Exponentiable {
-	val terms: scala.collection.mutable[Multiplicable]
+class MultNode extends MathObj with Algebraic{
+	val terms: scala.collection.mutable[Algebraic]
 }
 
-class ExpNode extends MathObj with Addable,
-								   Multiplicable,
-								   Exponentiable {
-	val terms: scala.collection.mutable[Exponentiable]
+class ExpNode extends MathObj with Algebraic {
+	val terms: scala.collection.mutable[Algebraic]
 }
 
-class NumNode extends MathObj with Addable,
-								   Multiplicable,
-								   Exponentiable {
+class NumNode extends MathObj with Algebraic{
 	val value: int
 }
 
-class IdNode extends MathObj with Addable,
-								  Multiplicable,
-								  Exponentiable {
+class IdNode extends MathObj with Algebraic{
 	val name: String
 }
 
-class Divnode extends mathObj with Addable,
-								   Multiplicable,
-								   Exponentiable {
-	val numerator: Multiplicable,
-	val denominator: Multiplicable
+class Divnode extends mathObj with Algebraic {
+	val numerator: Algebraic,
+	val denominator: Algebraic
 }
