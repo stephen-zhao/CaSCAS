fee424a0af7a674d243d9e34a3cffdea7711aef3
Yizhou Deng <yizhoudeng@gmail.com>
2017-09-15 21:51:32 -0400

    merging commit

--- src/main/scala/org/cascas_project/cascas/lang/builtin/BuiltIns.scala
+++ src/main/scala/org/cascas_project/cascas/lang/builtin/BuiltIns.scala
@@ -28,6 +28,48 @@ package object builtin {
       this.returnTpe
     )
     def apply(): TypedObject = TypedObject(tpe, obj)
+    
+    def typeCheck (ctx : Context) : Option[Evaluation] = {
+      var temp: Vector[Object] = Vector()
+      this.onApply(typeCheckRec(this.formalParams, ctx, temp), ctx)
+    }
+    
+    def typeCheckRec (fp : Vector[FormalParameter], ctx : Context, acc : Vector[Object]) : Vector[Object] = {
+      ctx.get(fp.head.id) match {
+        case Some(TypedObject(fp.head.tpe), v) => {
+          v.eval(ctx).keepOnlyReassignments match {
+            case Evaluation(l, ctxDelta) => {
+              acc = acc :+ l
+            }
+            case Evaluation(other, ctxDelta) => {
+              
+            }
+          }
+          if (fp.tail.length() > 0) {
+            typeCheckRec(fp.tail, ctx, acc)
+          } else {
+            acc
+          }
+        }
+        
+      case Some(TypedObject(other, _)) => {
+        // report type mismatch error
+        throw new Exception("type mismatch error") //TODO
+      }
+
+      // 3. case FAIL, is not assigned, then
+      case Some(other) => {
+        // report unassigned error
+        throw new Exception("is not assigned") //TODO
+      }
+      
+      // 4. case FAIL, is not defined, then
+      case None => {
+        // report undefined error
+        throw new Exception("is not defined") //TODO
+      }
+      }
+    }
   }
 
   trait BuiltInDefinitionWithCustomEval extends BuiltInDefinition {
